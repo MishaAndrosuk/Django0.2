0fab164e5a0a1dd6791d0fc1acea8a34b71fbdcf
Misha Androshchuk <androsukmisa3@gmail.com>
2025-01-23 20:57:10 +0200

    add admin

--- films/admin.py
+++ films/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from films.models import Film
+
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['title', 'year', 'director', 'rating', 'genre', 'runtime', 'description', 'photo']
+    search_fields = ['title', 'year', 'director', 'rating', 'genre', 'runtime', 'description', 'photo']
+    list_filter = ['title']
+
+admin.site.register(Film, UserAdmin)
+
+
